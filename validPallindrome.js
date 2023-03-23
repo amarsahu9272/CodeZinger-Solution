d184f3520673265ebcdb3df4f4fdc56c62bf4ca0
Anil Singh <anil927a@gmail.com>
2023-03-23 19:25:43 +0530

    validPallindrome.js

--- validPallindrome.js
+++ validPallindrome.js
@@ -1,7 +1,26 @@
-var isPalindrome = function (s) {
-  // Convert to lowercase and remove non-alphanumeric characters
+// var isPalindrome = function (s) {
+//   // Convert to lowercase and remove non-alphanumeric characters
+//   s = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+//   // Check if the string is the same forwards and backwards
+//   return s === s.split("").reverse().join("");
+// };
+// console.log(isPalindrome("A man, a plan, a canal: Panama"));
+
+function isPalindrome(s) {
+  // convert to lowercase and remove non-alphanumeric characters
   s = s.toLowerCase().replace(/[^a-z0-9]/g, "");
-  // Check if the string is the same forwards and backwards
-  return s === s.split("").reverse().join("");
-};
+
+  // base case
+  if (s.length <= 1) {
+    return true;
+  }
+
+  // recursive case
+  if (s[0] === s[s.length - 1]) {
+    return isPalindrome(s.slice(1, s.length - 1));
+  } else {
+    return false;
+  }
+}
+
 console.log(isPalindrome("A man, a plan, a canal: Panama"));
